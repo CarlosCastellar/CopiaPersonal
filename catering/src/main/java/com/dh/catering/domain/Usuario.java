f54a6fdbd74b01cdf14ab3ad9cf395f6135ebb15
Carlos <carlos_castellar@hotmail.com>
2023-05-29 22:36:28 -0500

    se cargan el proyecto avance Carlos a la rama dev

---
+++ catering/src/main/java/com/dh/catering/domain/Usuario.java
@@ -0,0 +1,49 @@
+package com.dh.catering.domain;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuario")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(nullable = false)
+    private String contrasena;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "rol_id")
+    private Rol rol;
+
+    public Usuario(String nombre, String apellido, String contrasena, String email, Rol rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.contrasena = contrasena;
+        this.email = email;
+        this.rol = rol;
+    }
+
+}
---
+++ catering/src/main/java/com/dh/catering/service/UsuarioService.java
@@ -0,0 +1,116 @@
+package com.dh.catering.service;
+
+import com.dh.catering.domain.Producto;
+import com.dh.catering.domain.Usuario;
+import com.dh.catering.dto.ProductoDto;
+import com.dh.catering.dto.UsuarioDto;
+import com.dh.catering.exceptions.DuplicadoException;
+import com.dh.catering.exceptions.NombreDuplicadoException;
+import com.dh.catering.exceptions.RecursoNoEncontradoException;
+import com.dh.catering.repository.RolRepository;
+import com.dh.catering.repository.UsuarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private static final String MSJ_EXITO = "Se guardo exitosamente el usuario";
+    private static final String MSJ_ERROR = "El correo '%s' ya se encuentra registrado en el sistema";
+    private final UsuarioRepository usuarioRepository;
+
+    private  final RolRepository rolRepository;
+    private final ObjectMapper mapper;
+
+    public Optional<String> save(UsuarioDto dto) throws DuplicadoException {
+        if (usuarioRepository.findByEmail(dto.getEmail()).isPresent()) {
+            throw new DuplicadoException(MSJ_ERROR.formatted(dto.getEmail()));
+        }
+        Usuario usuario = mapper.convertValue(dto, Usuario.class);
+        usuario.setRol(rolRepository.getByNombre(dto.getRolName()).get());
+        usuarioRepository.save(usuario);
+        log.info(MSJ_EXITO);
+        return Optional.of(MSJ_EXITO);
+    }
+
+    public List<UsuarioDto> listar() {
+        List<UsuarioDto> usuarioDtos = new ArrayList<>();
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        for (Usuario usuario: usuarios){
+            UsuarioDto usuarioDto = mapper.convertValue(usuario,UsuarioDto.class);
+            usuarioDto.setRolName(usuario.getRol().getNombre());
+            usuarioDtos.add(usuarioDto);
+        }
+        return usuarioDtos;
+    }
+
+    public List<UsuarioDto> findAllByRolId(Long id) {
+        List<UsuarioDto> usuarioDtos = new ArrayList<>();
+        List<Usuario> usuarios = usuarioRepository.findAllByRolId(id);
+        for (Usuario usuario:usuarios){
+            UsuarioDto usuarioDto = mapper.convertValue(usuario,UsuarioDto.class);
+            usuarioDto.setRolName(usuario.getRol().getNombre());
+            usuarioDtos.add(usuarioDto);
+        }
+        return usuarioDtos;
+    }
+
+    public Optional<UsuarioDto> getById(Long id) throws RecursoNoEncontradoException {
+        UsuarioDto usuarioDto = null;
+        Optional<Usuario> optionalUsuario = usuarioRepository.findById(id);
+        if (optionalUsuario.isEmpty()){
+            log.error("No existe un usuario con el id: " + id);
+            throw new RecursoNoEncontradoException("No existe un usuario con el id: " + id);
+        }
+        usuarioDto = mapper.convertValue(optionalUsuario.get(),UsuarioDto.class);
+        usuarioDto.setRolName(optionalUsuario.get().getRol().getNombre());
+        return  Optional.ofNullable(usuarioDto);
+    }
+
+    public Optional<UsuarioDto> getByEmail(String email) throws RecursoNoEncontradoException {
+        UsuarioDto usuarioDto = null;
+        Optional<Usuario> optionalUsuario = usuarioRepository.findByEmail(email);
+        if (optionalUsuario.isEmpty()){
+            log.error("No existe un usuario con el email: " + email);
+            throw new RecursoNoEncontradoException("No existe un usuario con el email: " + email);
+        }
+        usuarioDto = mapper.convertValue(optionalUsuario.get(),UsuarioDto.class);
+        usuarioDto.setRolName(optionalUsuario.get().getRol().getNombre());
+        return  Optional.ofNullable(usuarioDto);
+    }
+
+    public Optional<String> deleteById(Long id) throws RecursoNoEncontradoException {
+        String mensaje = null;
+        Optional<UsuarioDto> optionalUsuarioDto = this.getById(id);
+        if (optionalUsuarioDto.isPresent()) {
+            usuarioRepository.deleteById(id);
+            mensaje = "Se elimino exitosamente el usuario con id: " + id;
+            log.info(mensaje);
+        }
+        return Optional.ofNullable(mensaje);
+    }
+
+    public Optional<String> updateById(Long id, UsuarioDto usuarioDto) throws RecursoNoEncontradoException, DuplicadoException {
+        String mensaje = null;
+        this.deleteById(id);
+        this.save(usuarioDto);
+        mensaje = "Se actualizo correctamente el usuario que tenia el id: " + id + ". Al usuario actualizado se le asigno el id: " + this.getByEmail(usuarioDto.getEmail()).get().getId();
+        log.info(mensaje);
+        return Optional.ofNullable(mensaje);
+    }
+
+}
